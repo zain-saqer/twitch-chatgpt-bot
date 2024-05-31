3763bd4be10e7f5f9f0934927337a187c0d488f7
zain <zainsaqer@gmail.com>
2024-05-31 10:14:04 +0200

    chatgpt chat completion api

---
+++ internal/chatgpt/api.go
@@ -0,0 +1,82 @@
+package chatgpt
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type API struct {
+	client        *http.Client
+	systemMessage string
+	model         string
+	apiKey        string
+}
+
+func NewAPI(client *http.Client, systemMessage, model, apiKey string) *API {
+	return &API{client: client, systemMessage: systemMessage, model: model, apiKey: apiKey}
+}
+
+type message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type completion struct {
+	Model    string     `json:"model"`
+	Messages []*message `json:"messages"`
+}
+
+type choice struct {
+	Index   int      `json:"index"`
+	Message *message `json:"message"`
+}
+
+type completionObject struct {
+	Choices []*choice `json:"choices"`
+}
+
+func (a *API) completions(q string) (answer string, err error) {
+	messages := []*message{
+		{Role: "system", Content: a.systemMessage},
+		{Role: "system", Content: q},
+	}
+	completion := &completion{Model: a.model, Messages: messages}
+	bodyBytes, err := json.Marshal(completion)
+	if err != nil {
+		return "", err
+	}
+	body := bytes.NewReader(bodyBytes)
+	request, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", body)
+	if err != nil {
+		return "", err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bearer "+a.apiKey)
+	response, err := a.client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		_err := Body.Close()
+		if _err != nil {
+			err = _err
+		}
+	}(response.Body)
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf(`openai completions api non ok response: %s`, response.Status)
+	}
+	responseBytes, err := io.ReadAll(response.Body)
+	completionObj := &completionObject{}
+	err = json.Unmarshal(responseBytes, completionObj)
+	if err != nil {
+		return "", err
+	}
+	if len(completionObj.Choices) == 0 {
+		return "", fmt.Errorf(`0 choices returned from openai completions endpoint`)
+	}
+	message := completionObj.Choices[0].Message
+	return message.Content, nil
+}
---
+++ internal/chatgpt/api_test.go
@@ -0,0 +1,23 @@
+package chatgpt
+
+import (
+	"github.com/zain-saqer/twitch-chatgpt/internal/env"
+	"net/http"
+	"testing"
+)
+
+func TestApiChatCompletion(t *testing.T) {
+	apiKey, err := env.GetEnv(`OPENAI_API_KEY`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	api := NewAPI(&http.Client{}, `repeat exactly what the user say`, `gpt-3.5-turbo`, apiKey)
+	q := `ping 123`
+	answer, err := api.completions(q)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if answer != q {
+		t.Errorf("got %q, want %q", answer, q)
+	}
+}
